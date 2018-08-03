e3b26bdbb3680d750de89bff04729eed877967b9
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2018-08-03 00:05:11 -0300

    Init

---
+++ main.js
@@ -0,0 +1,6 @@
+window.onload = (_ => {
+  console.log('Hi.');
+  console.log('Here is where you can get my music:');
+  console.log('https://soundcloud.com/muniz95/tracks');
+  console.log('https://imateapot.bandcamp.com');
+})();
